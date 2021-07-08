979f0189c38d1a2f81fdbb99824d89c3d8ebb1d0
LastSkywalkerER <maxdr1998@gmail.com>
2021-07-08 22:13:41 +0300

    Добавьте файлы проекта.

---
+++ Class1.cs
@@ -0,0 +1,27 @@
+﻿using Autodesk.AutoCAD.Runtime;
+using Autodesk.AutoCAD.ApplicationServices;
+using Autodesk.AutoCAD.EditorInput;
+using Autodesk.AutoCAD.DatabaseServices;
+using Autodesk.AutoCAD.Geometry;
+
+using Autodesk.AutoCAD.Windows;
+
+using System.Runtime.InteropServices;
+
+namespace ACADuserInterface
+{
+	[Assembly: ExtensionApplication(GetType(adskClass))]
+	public class adskClass : IExtensionApplication
+	{
+		ContextMenuExtension myContextMenu;
+		public void Initialize()
+		{
+			throw new System.NotImplementedException();
+		}
+
+		public void Terminate()
+		{
+			throw new System.NotImplementedException();
+		}
+	}
+}
